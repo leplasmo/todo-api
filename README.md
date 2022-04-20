dff9b44cf71b343a59b0790c786d48211ce6ea3b
Marcel Thiele <marcel.thiele55@gmail.com>
2022-04-20 12:19:16 +0200

    cli api done

--- README.md
+++ README.md
@@ -3,11 +3,26 @@
 ## Usage
 
 ```bash
-cd cmd/todo
 
-# list existing todos
-go run main.go
+# set the json file to write to
+export JSON_FILENAME=my-todos.json
 
 # add a todo
-go run main.go This is a new todo
+go run cmd/todo/main.go -add "This is a new todo"
+
+# or, you can add a todo by providing an interactive input
+go run cmd/todo/main.go -add
+This is another todo
+
+# or, you can also pipe STDIN to add a new todo 
+echo "This is yet another todo" | go run cmd/todo/main.go -add
+
+# list todos
+go run cmd/todo/main.go -list
+#OUTPUT:
+'
+[ ]  1: This is a new todo
+[ ]  2: This is another todo
+[ ]  3: This is yet another todo
+'
 ```
--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,18 +1,40 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"io"
 	"os"
+	"strings"
 
 	"github.com/leplasmo/todo-api"
 )
 
 var todoFileName = ".todo.json"
 
+func getTask(r io.Reader, args ...string) (string, error) {
+	if len(args) > 0 {
+		return strings.Join(args, " "), nil
+	}
+
+	s := bufio.NewScanner(r)
+	s.Scan()
+
+	if err := s.Err(); err != nil {
+		return "", err
+	}
+
+	if len(s.Text()) == 0 {
+		return "", fmt.Errorf("Task cannot be blank")
+	}
+
+	return s.Text(), nil
+}
+
 func main() {
 	// parse command line flags
-	task := flag.String("task", "", "Task to be included in the TODO list")
+	add := flag.Bool("add", false, "Task to be included in the TODO list")
 	list := flag.Bool("list", false, "List all TODOs")
 	complete := flag.Int("complete", 0, "TODO item to mark as completed")
 
@@ -44,8 +66,15 @@ func main() {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
 		}
-	case *task != "":
-		l.Add(*task)
+	case *add:
+		// When any arguments (excluding flags) are provided
+		// they will be used as the new task
+		t, err := getTask(os.Stdin, flag.Args()...)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		l.Add(t)
 		if err := l.Save(todoFileName); err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
